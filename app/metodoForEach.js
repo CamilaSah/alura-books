76db923172868ef90f9a0b051d991a204d343491
Camila Sayuri <tokubo.camila@gmail.com>
2023-10-26 14:35:10 -0300

    adicionando filtros e ordenação por preços

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,10 +1,13 @@
 const elementoParaInserirLivros = document.getElementById('livros');
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
             <h2 class="livro__titulo">
             ${livro.titulo}
             </h2>
@@ -17,3 +20,11 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         ` 
     })
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//     if (livro.quantidade > 0) {
+//         return 'livro__imagens';
+//     } else {
+//         return 'livro__imagens indisponivel';
+//     }
+// }
